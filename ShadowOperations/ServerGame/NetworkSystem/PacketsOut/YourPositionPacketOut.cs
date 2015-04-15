20cc0d282ce19ac58bb25e0facf75524d9a2ee57
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2015-04-14 18:02:59 -0700

    every time, git, every time

---
+++ ShadowOperations/ServerGame/NetworkSystem/PacketsOut/YourPositionPacketOut.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShadowOperations.Shared;
+
+namespace ShadowOperations.ServerGame.NetworkSystem.PacketsOut
+{
+    public class YourPositionPacketOut: AbstractPacketOut
+    {
+        public YourPositionPacketOut(Location pos)
+        {
+            ID = 1;
+            Data = pos.ToBytes();
+        }
+    }
+}
