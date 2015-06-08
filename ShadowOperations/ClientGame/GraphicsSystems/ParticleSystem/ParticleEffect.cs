5aa016a0f4513a7fdeb0f99e8fc924e26368a877
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2015-06-08 05:16:46 -0700

    Add the first classes that will eventually be a particle system

---
+++ ShadowOperations/ClientGame/GraphicsSystems/ParticleSystem/ParticleEffect.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShadowOperations.ClientGame.ClientMainSystem;
+
+namespace ShadowOperations.ClientGame.GraphicsSystems.ParticleSystem
+{
+    public abstract class ParticleEffect
+    {
+        public Client TheClient;
+
+        public ParticleEffect(Client tclient)
+        {
+            TheClient = tclient;
+        }
+
+        public abstract void Render();
+
+        public abstract ParticleEffect Clone();
+    }
+}
---
+++ ShadowOperations/ClientGame/GraphicsSystems/ParticleSystem/ParticleEngine.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShadowOperations.ClientGame.GraphicsSystems.ParticleSystem
+{
+    public class ParticleEngine
+    {
+    }
+}
