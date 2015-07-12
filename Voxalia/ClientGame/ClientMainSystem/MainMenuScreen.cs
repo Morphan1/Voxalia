a648be0c86295a632df7f7708c5e0e258d4c5ae7
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2015-07-11 22:45:51 -0700

    Add a menu topper image

--- Voxalia/ClientGame/ClientMainSystem/MainMenuScreen.cs
+++ Voxalia/ClientGame/ClientMainSystem/MainMenuScreen.cs
@@ -17,17 +17,20 @@ namespace Voxalia.ClientGame.ClientMainSystem
 
         public Texture Mouse;
 
+        public Texture Topper;
+
         public override void Init()
         {
             Menus = new UIMenu(TheClient);
             Menus.Add(new UIMenuButton("ui/menus/buttons/basic", "Singleplayer", () => {
                 UIConsole.WriteLine("SP!");
-            }, 10, 100, 350, 70, TheClient.FontSets.SlightlyBigger));
+            }, 10, 300, 350, 70, TheClient.FontSets.SlightlyBigger));
             Menus.Add(new UIMenuButton("ui/menus/buttons/basic", "Multiplayer", () =>
             {
                 UIConsole.WriteLine("MP!");
-            }, 10, 200, 350, 70, TheClient.FontSets.SlightlyBigger));
+            }, 10, 400, 350, 70, TheClient.FontSets.SlightlyBigger));
             Mouse = TheClient.Textures.GetTexture("ui/mouse_cursor");
+            Topper = TheClient.Textures.GetTexture("ui/menus/voxalia_topper");
         }
 
         public override void Tick()
@@ -45,6 +48,8 @@ namespace Voxalia.ClientGame.ClientMainSystem
             TheClient.Establish2D();
             GL.ClearBuffer(ClearBuffer.Color, 0, new float[] { 0, 0.5f, 0.5f, 1 });
             GL.ClearBuffer(ClearBuffer.Depth, 0, new float[] { 1 });
+            Topper.Bind();
+            TheClient.Rendering.RenderRectangle(0, 0, TheClient.Window.Width, TheClient.Window.Width / 2);
             Menus.RenderAll(TheClient.gDelta);
             Mouse.Bind();
             TheClient.Rendering.RenderRectangle(MouseHandler.MouseX(), MouseHandler.MouseY(), MouseHandler.MouseX() + 16, MouseHandler.MouseY() + 16);
