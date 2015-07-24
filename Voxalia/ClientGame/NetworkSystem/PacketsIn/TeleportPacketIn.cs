ca770391618564c77a6816b0f62c362895aeb748
Morphan1 <morphanone@gmail.com>
2015-07-24 03:37:24 -0400

    Add Teleport packet

---
+++ Voxalia/ClientGame/NetworkSystem/PacketsIn/TeleportPacketIn.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Voxalia.Shared;
+
+namespace Voxalia.ClientGame.NetworkSystem.PacketsIn
+{
+    public class TeleportPacketIn : AbstractPacketIn
+    {
+        public override bool ParseBytesAndExecute(byte[] data)
+        {
+            if (data.Length != 12)
+            {
+                return false;
+            }
+            Location pos = Location.FromBytes(data, 0);
+            TheClient.Player.SetPosition(pos);
+            return true;
+        }
+    }
+}
---
+++ Voxalia/ServerGame/NetworkSystem/PacketsOut/TeleportPacketOut.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Voxalia.Shared;
+using BEPUutilities;
+
+namespace Voxalia.ServerGame.NetworkSystem.PacketsOut
+{
+    public class TeleportPacketOut : AbstractPacketOut
+    {
+        public TeleportPacketOut(Location pos)
+        {
+            ID = 27;
+            Data = new byte[12];
+            pos.ToBytes().CopyTo(Data, 0);
+        }
+    }
+}
