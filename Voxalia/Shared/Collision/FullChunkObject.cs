e211c44c907f52fa0fa7139799296f7a924fae0e
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2015-08-09 03:25:47 -0700

    some placeholder code for eventually having custom chunk physics objects

---
+++ Voxalia/Shared/Collision/FullChunkObject.cs
@@ -0,0 +1,63 @@
+﻿using BEPUphysics.BroadPhaseEntries;
+using BEPUphysics.CollisionShapes.ConvexShapes;
+using BEPUutilities;
+using BEPUphysics.BroadPhaseEntries.Events;
+using BEPUphysics.OtherSpaceStages;
+
+namespace Voxalia.Shared.Collision
+{
+    public class FullChunkObject : StaticCollidable
+    {
+        public FullChunkObject(Vector3 pos, BlockInternal[] blocks)
+        {
+            ChunkShape = new FullChunkShape(blocks);
+            Position = pos;
+        }
+
+        public FullChunkShape ChunkShape;
+
+        public Vector3 Position;
+
+        public ContactEventManager<FullChunkObject> Events = new ContactEventManager<FullChunkObject>();
+
+        protected override IContactEventTriggerer EventTriggerer
+        {
+            get { return Events; }
+        }
+
+        protected override IDeferredEventCreator EventCreator
+        {
+            get { return Events; }
+        }
+
+        public override void UpdateBoundingBox()
+        {
+            boundingBox = new BoundingBox(Position, Position + new Vector3(30, 30, 30));
+        }
+
+        public override bool ConvexCast(ConvexShape castShape, ref RigidTransform startingTransform, ref Vector3 sweep, out RayHit hit)
+        {
+            // TODO: Actual code, less cheating!
+            Vector3 move = sweep - startingTransform.Position;
+            float movelen = move.Length();
+            Ray testray = new Ray(startingTransform.Position, move / movelen);
+            return RayCast(testray, movelen, out hit);
+        }
+
+        public override bool RayCast(Ray ray, float maximumLength, out RayHit rayHit)
+        {
+            Ray r2 = new Ray(ray.Position - Position, ray.Direction);
+            if (ChunkShape.RayCast(ref r2, maximumLength, out rayHit))
+            {
+                rayHit.Location = rayHit.Location + ray.Position;
+                rayHit.T = (rayHit.Location - r2.Position).Length() / maximumLength; // TODO: Determine if needed at all. Length()'s are bad!
+                // TODO: ALTERNATELY, find a quicker calculation for T.
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Voxalia/Shared/Collision/FullChunkShape.cs
@@ -0,0 +1,148 @@
+﻿using BEPUphysics.CollisionShapes;
+using BEPUutilities;
+using System;
+
+namespace Voxalia.Shared.Collision
+{
+    public class FullChunkShape : CollisionShape
+    {
+        public const int CHUNK_SIZE = 30;
+
+        public FullChunkShape(BlockInternal[] blocks)
+        {
+            Blocks = blocks;
+        }
+
+        public BlockInternal[] Blocks = null;
+
+        public int BlockIndex(int x, int y, int z)
+        {
+            return z * CHUNK_SIZE * CHUNK_SIZE + y * CHUNK_SIZE + x;
+        }
+
+        /// <summary>
+        /// Performs a raycast.
+        /// NOTE: hit.T is always 0.
+        /// </summary>
+        public bool RayCast(ref Ray ray, float maximumLength, out RayHit hit)
+        {
+            hit = new RayHit();
+            float x = (float)Math.Floor(ray.Position.X);
+            float y = (float)Math.Floor(ray.Position.Y);
+            float z = (float)Math.Floor(ray.Position.Z);
+            float dx = ray.Direction.X;
+            float dy = ray.Direction.Y;
+            float dz = ray.Direction.Z;
+            float stepX = signum(dx);
+            float stepY = signum(dy);
+            float stepZ = signum(dz);
+            float tMaxX = intbound(ray.Position.X, dx);
+            float tMaxY = intbound(ray.Position.Y, dy);
+            float tMaxZ = intbound(ray.Position.Z, dz);
+            float tDeltaX = stepX / dx;
+            float tDeltaY = stepY / dy;
+            float tDeltaZ = stepZ / dz;
+            Vector3 face = new Vector3();
+            if (dx == 0 && dy == 0 && dz == 0)
+            {
+                // Invalid ray?!
+                return false;
+            }
+            while ((stepX > 0 ? x < CHUNK_SIZE : x >= 0) &&
+                (stepY > 0 ? y < CHUNK_SIZE : y >= 0) &&
+                (stepZ > 0 ? z < CHUNK_SIZE : z >= 0))
+            {
+                if (!(x < 0 || y < 0 || z < 0 || x >= CHUNK_SIZE || y >= CHUNK_SIZE || z >= CHUNK_SIZE))
+                {
+                    BlockInternal bi = Blocks[BlockIndex((int)x, (int)y, (int)z)];
+                    if (((Material)bi.BlockMaterial).IsSolid())
+                    {
+                        // TODO: Trace into the block shape if custom shaped!
+                        hit.Normal = face;
+                        hit.Location = new Vector3(x, y, z);
+                        hit.T = 0;
+                        return true;
+                    }
+                }
+                if (tMaxX < tMaxY)
+                {
+                    if (tMaxX < tMaxZ)
+                    {
+                        if (tMaxX > maximumLength)
+                        {
+                            break;
+                        }
+                        x += stepX;
+                        tMaxX += tDeltaX;
+                        face.X = -stepX;
+                        face.Y = 0;
+                        face.Z = 0;
+                    }
+                    else
+                    {
+                        if (tMaxZ > maximumLength)
+                        {
+                            break;
+                        }
+                        z += stepZ;
+                        tMaxZ += tDeltaZ;
+                        face.X = 0;
+                        face.Y = 0;
+                        face.Z = -stepZ;
+                    }
+                }
+                else
+                {
+                    if (tMaxY < tMaxZ)
+                    {
+                        if (tMaxY > maximumLength)
+                        {
+                            break;
+                        }
+                        y += stepY;
+                        tMaxY += tDeltaY;
+                        face.X = 0;
+                        face.Y = -stepY;
+                        face.Z = 0;
+                    }
+                    else
+                    {
+                        if (tMaxZ > maximumLength)
+                        {
+                            break;
+                        }
+                        z += stepZ;
+                        tMaxZ += tDeltaZ;
+                        face.X = 0;
+                        face.Y = 0;
+                        face.Z = -stepZ;
+                    }
+                }
+            }
+            return false;
+        }
+
+        float intbound(float s, float ds)
+        {
+            if (ds < 0)
+            {
+                return intbound(-s, -ds);
+            }
+            else
+            {
+                s = mod(s, 1);
+                return (1 - s) / ds;
+            }
+        }
+
+        float signum(float x)
+        {
+            return x > 0 ? 1 : x < 0 ? -1 : 0;
+        }
+
+        float mod(float value, float modulus)
+        {
+            return (value % modulus + modulus) % modulus;
+        }
+    }
+}
