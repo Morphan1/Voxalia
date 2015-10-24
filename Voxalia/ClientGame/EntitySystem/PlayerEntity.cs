6dbb61e54c1a22271e54b63e36c9590e2b7a2fbe
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2015-10-23 21:39:47 -0700

    tweak some variables

--- Voxalia/ClientGame/EntitySystem/PlayerEntity.cs
+++ Voxalia/ClientGame/EntitySystem/PlayerEntity.cs
@@ -210,7 +210,7 @@ namespace Voxalia.ClientGame.EntitySystem
             }
             // TODO: Better variable control! (Server should command every detail!)
             CBody = new CharacterController(ServerLocation.ToBVector(), (float)HalfSize.Z * 2f, (float)HalfSize.Z * 1.1f,
-                (float)HalfSize.X, 0.01f, 10f, 1.0f, 1.3f, 8f, 3f, 1000f, 5f, 50f, 0.5f, 250f, 4.5f, 3f, 5000f);
+                (float)HalfSize.X, 0.01f, 10f, 1.0f, 1.3f, 5f, 2.5f, 1000f, 5f, 50f, 0.5f, 250f, 4.5f, 2.5f, 5000f);
             CBody.StanceManager.DesiredStance = Stance.Standing;
             CBody.ViewDirection = new Vector3(1f, 0f, 0f);
             CBody.Down = new Vector3(0f, 0f, -1f);
