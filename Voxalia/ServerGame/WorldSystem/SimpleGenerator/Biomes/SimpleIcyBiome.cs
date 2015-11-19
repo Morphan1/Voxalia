12c541c13ad37cbedba77e99ab99e403e99028c9
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2015-11-18 23:22:31 -0800

    snow biomes get snow!

--- Voxalia/ServerGame/WorldSystem/SimpleGenerator/Biomes/SimpleIcyBiome.cs
+++ Voxalia/ServerGame/WorldSystem/SimpleGenerator/Biomes/SimpleIcyBiome.cs
@@ -1,10 +1,22 @@
-﻿namespace Voxalia.ServerGame.WorldSystem.SimpleGenerator.Biomes
+﻿using Voxalia.Shared;
+
+namespace Voxalia.ServerGame.WorldSystem.SimpleGenerator.Biomes
 {
     public class SimpleIcyBiome: SimpleBiome
     {
         public override string GetName()
         {
             return "Icy";
+        }
+
+        public override Material SurfaceBlock()
+        {
+            return Material.SNOW; // TODO: Ice?
+        }
+
+        public override Material SecondLayerBlock()
+        {
+            return Material.SNOW;
         }
     }
 }
--- Voxalia/ServerGame/WorldSystem/SimpleGenerator/Biomes/SimpleSnowBiome.cs
+++ Voxalia/ServerGame/WorldSystem/SimpleGenerator/Biomes/SimpleSnowBiome.cs
@@ -1,10 +1,22 @@
-﻿namespace Voxalia.ServerGame.WorldSystem.SimpleGenerator.Biomes
+﻿using Voxalia.Shared;
+
+namespace Voxalia.ServerGame.WorldSystem.SimpleGenerator.Biomes
 {
     public class SimpleSnowBiome: SimpleBiome
     {
         public override string GetName()
         {
             return "Snow";
+        }
+
+        public override Material SurfaceBlock()
+        {
+            return Material.SNOW;
+        }
+
+        public override Material SecondLayerBlock()
+        {
+            return Material.SNOW;
         }
     }
 }
