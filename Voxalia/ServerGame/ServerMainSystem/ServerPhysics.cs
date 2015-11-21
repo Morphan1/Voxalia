fca28a9b4e070b369cc508941ea9abc3fe8665e0
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2015-11-20 18:03:44 -0800

    Add regionLoadEvent (between existing pre and post events)

--- Voxalia/ServerGame/ServerMainSystem/ServerPhysics.cs
+++ Voxalia/ServerGame/ServerMainSystem/ServerPhysics.cs
@@ -19,6 +19,13 @@ namespace Voxalia.ServerGame.ServerMainSystem
         public FreneticEventHandler<RegionLoadPreEventArgs> OnRegionLoadPreEvent = new FreneticEventHandler<RegionLoadPreEventArgs>();
 
         /// <summary>
+        /// Fired when a region is loaded and is going to be added; can be cancelled.
+        /// For purely listening to a region load after the fact, use <see cref="OnRegionLoadPostEvent"/>.
+        /// TODO: Move to an event helper!
+        /// </summary>
+        public FreneticEventHandler<RegionLoadEventArgs> OnRegionLoadEvent = new FreneticEventHandler<RegionLoadEventArgs>();
+
+        /// <summary>
         /// Fired when a region has been loaded; is purely informative.
         /// For cancelling a region load, use <see cref="OnRegionLoadPreEvent"/>.
         /// TODO: Move to an event helper!
@@ -45,6 +52,13 @@ namespace Voxalia.ServerGame.ServerMainSystem
             region.Name = nl;
             region.TheServer = this;
             region.BuildWorld();
+            RegionLoadEventArgs e2 = new RegionLoadEventArgs() { TheRegion = region };
+            OnRegionLoadEvent.Fire(e2);
+            if (e.Cancelled)
+            {
+                region.UnloadFully();
+                return null;
+            }
             LoadedRegions.Add(region);
             OnRegionLoadPostEvent.Fire(new RegionLoadPostEventArgs() { TheRegion = region });
             return region;
@@ -60,6 +74,13 @@ namespace Voxalia.ServerGame.ServerMainSystem
                 LoadedRegions[i].Tick(delta);
             }
         }
+    }
+
+    public class RegionLoadEventArgs : EventArgs
+    {
+        public bool Cancelled = false;
+
+        public Region TheRegion = null;
     }
 
     public class RegionLoadPreEventArgs : EventArgs
