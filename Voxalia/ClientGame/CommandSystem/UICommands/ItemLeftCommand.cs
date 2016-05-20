4f62d36933d68f0c578d7e1a05284caf1438a8bb
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2016-05-19 22:21:22 -0700

    follow up commit

---
+++ Voxalia/ClientGame/CommandSystem/UICommands/ItemLeftCommand.cs
@@ -0,0 +1,38 @@
+﻿using FreneticScript.CommandSystem;
+using Voxalia.ClientGame.ClientMainSystem;
+
+namespace Voxalia.ClientGame.CommandSystem.UICommands
+{
+    class ItemleftCommand : AbstractCommand
+    {
+        public Client TheClient;
+
+        public ItemleftCommand(Client tclient)
+        {
+            TheClient = tclient;
+            Name = "itemleft";
+            Description = "Adjust the item (left version).";
+            Arguments = "";
+        }
+
+        public override void Execute(CommandQueue queue, CommandEntry entry)
+        {
+            if (entry.Marker == 0)
+            {
+                entry.Bad(queue, "Must use +, -, or !");
+            }
+            else if (entry.Marker == 1)
+            {
+                TheClient.Player.ItemLeft = true;
+            }
+            else if (entry.Marker == 2)
+            {
+                TheClient.Player.ItemLeft = false;
+            }
+            else if (entry.Marker == 3)
+            {
+                TheClient.Player.ItemLeft = !TheClient.Player.ItemLeft;
+            }
+        }
+    }
+}
---
+++ Voxalia/ClientGame/CommandSystem/UICommands/ItemUpCommand.cs
@@ -0,0 +1,38 @@
+﻿using FreneticScript.CommandSystem;
+using Voxalia.ClientGame.ClientMainSystem;
+
+namespace Voxalia.ClientGame.CommandSystem.UICommands
+{
+    class ItemupCommand : AbstractCommand
+    {
+        public Client TheClient;
+        
+        public ItemupCommand(Client tclient)
+        {
+            TheClient = tclient;
+            Name = "itemup";
+            Description = "Adjust the item (up version).";
+            Arguments = "";
+        }
+
+        public override void Execute(CommandQueue queue, CommandEntry entry)
+        {
+            if (entry.Marker == 0)
+            {
+                entry.Bad(queue, "Must use +, -, or !");
+            }
+            else if (entry.Marker == 1)
+            {
+                TheClient.Player.ItemUp = true;
+            }
+            else if (entry.Marker == 2)
+            {
+                TheClient.Player.ItemUp = false;
+            }
+            else if (entry.Marker == 3)
+            {
+                TheClient.Player.ItemUp = !TheClient.Player.ItemUp;
+            }
+        }
+    }
+}
