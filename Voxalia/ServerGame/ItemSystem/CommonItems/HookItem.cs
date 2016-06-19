71dc45cda09f9f117df39c6478a38f27fe132d71
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2016-06-18 21:30:30 -0700

    Really low quality joint extend/retract
    This needs fixing

--- Voxalia/ServerGame/ItemSystem/CommonItems/HookItem.cs
+++ Voxalia/ServerGame/ItemSystem/CommonItems/HookItem.cs
@@ -33,21 +33,26 @@ namespace Voxalia.ServerGame.ItemSystem.CommonItems
             {
                 return;
             }
+            ApplyHook(player, item, cr.Position, cr.HitEnt);
+        }
+
+        public void ApplyHook(PlayerEntity player, ItemStack item, Location Position, BEPUphysics.Entities.Entity HitEnt)
+        {
             RemoveHook(player);
             PhysicsEntity pe;
-            float len = (float)(cr.Position - player.GetCenter()).Length();
-            Location step = (player.GetCenter() - cr.Position) / len;
+            float len = (float)(Position - player.GetCenter()).Length();
+            Location step = (player.GetCenter() - Position) / len;
             Location forw = Utilities.VectorToAngles(step);
             BEPUutilities.Quaternion quat = Quaternion.CreateFromAxisAngle(new Vector3(1, 0, 0), (float)(forw.Pitch * Utilities.PI180)) *
                 Quaternion.CreateFromAxisAngle(new Vector3(0, 0, 1), (float)(forw.Yaw * Utilities.PI180));
-            if (cr.HitEnt == null)
+            if (HitEnt == null)
             {
                 ModelEntity mod = new ModelEntity("cube", player.TheRegion);
                 mod.Mass = 0;
                 mod.CanSave = false;
                 mod.scale = new Location(0.023, 0.05, 0.05);
                 mod.mode = ModelCollisionMode.AABB;
-                mod.SetPosition(cr.Position);
+                mod.SetPosition(Position);
                 mod.SetOrientation(quat);
                 player.TheRegion.SpawnEntity(mod);
                 pe = mod;
@@ -55,16 +60,19 @@ namespace Voxalia.ServerGame.ItemSystem.CommonItems
             }
             else
             {
-                pe = (PhysicsEntity)cr.HitEnt.Tag;
+                pe = (PhysicsEntity)HitEnt.Tag;
             }
-            BaseJoint jd;
-            jd = new JointDistance(player, pe, 0.01f, len + 0.01f, player.GetCenter(), cr.Position);
+            JointDistance jd;
+            jd = new JointDistance(player, pe, 0.01f, len + 0.01f, player.GetCenter(), Position);
             player.TheRegion.AddJoint(jd);
             player.Hooks.Add(new HookInfo() { Joint = jd, Hit = pe, IsBar = false });
+
+
+
             PhysicsEntity cent = pe;
             for (float f = 0; f < len - 1f; f += 0.5f)
             {
-                Location cpos = cr.Position + step * f;
+                Location cpos = Position + step * f;
                 ModelEntity ce = new ModelEntity("cube", player.TheRegion);
                 ce.Mass = 1;
                 ce.CanSave = false;
@@ -73,13 +81,13 @@ namespace Voxalia.ServerGame.ItemSystem.CommonItems
                 ce.SetPosition(cpos + step * 0.5);
                 ce.SetOrientation(quat);
                 player.TheRegion.SpawnEntity(ce);
-                jd = new JointDistance(ce, cent, 0.01f, 0.5f, ce.GetPosition(), (ReferenceEquals(cent, pe) ? cr.Position: cent.GetPosition()));
+                jd = new JointDistance(ce, cent, 0.01f, 0.5f, ce.GetPosition(), (ReferenceEquals(cent, pe) ? Position: cent.GetPosition()));
                 CollisionRules.AddRule(player.Body, ce.Body, CollisionRule.NoBroadPhase);
                 player.TheRegion.AddJoint(jd);
                 player.Hooks.Add(new HookInfo() { Joint = jd, Hit = ce, IsBar = true });
                 cent = ce;
             }
-            jd = new JointDistance(cent, player, 0.01f, 1f, cent.GetPosition(), player.GetCenter());
+            jd = new JointDistance(cent, player, 0.01f, 0.5f, cent.GetPosition(), player.GetCenter());
             player.TheRegion.AddJoint(jd);
             player.Hooks.Add(new HookInfo() { Joint = jd, Hit = player, IsBar = false });
         }
@@ -145,15 +153,65 @@ namespace Voxalia.ServerGame.ItemSystem.CommonItems
         {
         }
 
+        public void Shrink(PlayerEntity player)
+        {
+            // TODO: Even out the magic player joint.
+            for (int i = 0; i < player.Hooks.Count; i++)
+            {
+                if (player.Hooks[i].Joint != null)
+                {
+                    if (player.Hooks[i].Joint.One == player || player.Hooks[i].Joint.Max > 0.3f)
+                    {
+                        player.Hooks[i].Joint.Max *= 0.9f;
+                        // TODO: less lazy networking of this
+                        player.TheRegion.DestroyJoint(player.Hooks[i].Joint);
+                        player.TheRegion.AddJoint(player.Hooks[i].Joint);
+                    }
+                }
+            }
+        }
+
+        public void Extend(PlayerEntity player)
+        {
+            // TODO: Even out the magic player joint.
+            for (int i = 0; i < player.Hooks.Count; i++)
+            {
+                if (player.Hooks[i].Joint != null)
+                {
+                    if (player.Hooks[i].Joint.One == player || player.Hooks[i].Joint.Max < 3f)
+                    {
+                        player.Hooks[i].Joint.Max *= 1.1f;
+                        // TODO: less lazy networking of this
+                        player.TheRegion.DestroyJoint(player.Hooks[i].Joint);
+                        player.TheRegion.AddJoint(player.Hooks[i].Joint);
+                    }
+                }
+            }
+        }
+
         public override void Tick(Entity entity, ItemStack item)
         {
+            if (!(entity is PlayerEntity))
+            {
+                // TODO: non-player support
+                return;
+            }
+            PlayerEntity player = (PlayerEntity)entity;
+            if (player.ItemLeft && !player.WasItemLefting && player.Hooks.Count > 0)
+            {
+                Extend(player);
+            }
+            if (player.ItemRight && !player.WasItemRighting && player.Hooks.Count > 0)
+            {
+                Shrink(player);
+            }
         }
     }
 
     public class HookInfo
     {
         public PhysicsEntity Hit = null;
-        public BaseJoint Joint = null;
+        public JointDistance Joint = null;
         public bool IsBar = false;
     }
 }
