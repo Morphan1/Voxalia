fb06996437ee2d817930ef7d2b9c146a4287517a
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2016-06-30 04:20:50 -0700

    fix hellstone item, make players less overly fat
    need to account for outstretched arms now, probably

--- Voxalia/ServerGame/EntitySystem/CharacterEntity.cs
+++ Voxalia/ServerGame/EntitySystem/CharacterEntity.cs
@@ -328,7 +328,7 @@ namespace Voxalia.ServerGame.EntitySystem
 
         public float CBDownStepHeight = 0.6f;
 
-        public float CBRadius = 0.57f;
+        public float CBRadius = 0.3f;
 
         public float CBMaxTractionSlope = 1.0f;
 
