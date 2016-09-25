2a14dc7980ffdc772281d5f130c21e5c1c14154c
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2016-09-24 23:48:13 -0700

    First pass at a launcher program

---
+++ Voxalia/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace VoxaliaLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loggedAs = new System.Windows.Forms.Label();
+            this.changeLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.playButton = new System.Windows.Forms.Button();
+            this.logoutButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // loggedAs
+            // 
+            this.loggedAs.AutoSize = true;
+            this.loggedAs.Location = new System.Drawing.Point(13, 13);
+            this.loggedAs.Name = "loggedAs";
+            this.loggedAs.Size = new System.Drawing.Size(61, 13);
+            this.loggedAs.TabIndex = 0;
+            this.loggedAs.Text = "Logged out";
+            // 
+            // changeLogin
+            // 
+            this.changeLogin.Location = new System.Drawing.Point(439, 40);
+            this.changeLogin.Name = "changeLogin";
+            this.changeLogin.Size = new System.Drawing.Size(101, 23);
+            this.changeLogin.TabIndex = 1;
+            this.changeLogin.Text = "Change Login";
+            this.changeLogin.UseVisualStyleBackColor = true;
+            this.changeLogin.Click += new System.EventHandler(this.changeLogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(16, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username:";
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Location = new System.Drawing.Point(80, 42);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.Size = new System.Drawing.Size(124, 20);
+            this.usernameBox.TabIndex = 3;
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(272, 42);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(161, 20);
+            this.passwordBox.TabIndex = 4;
+            this.passwordBox.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password:";
+            // 
+            // playButton
+            // 
+            this.playButton.Enabled = false;
+            this.playButton.Location = new System.Drawing.Point(12, 321);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(540, 23);
+            this.playButton.TabIndex = 6;
+            this.playButton.Text = "Play Voxalia";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.Location = new System.Drawing.Point(439, 3);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(101, 23);
+            this.logoutButton.TabIndex = 7;
+            this.logoutButton.Text = "Log Out";
+            this.logoutButton.UseVisualStyleBackColor = true;
+            this.logoutButton.Click += new System.EventHandler(this.logoutButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Enabled = false;
+            this.progressBar1.Location = new System.Drawing.Point(19, 69);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(521, 10);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 356);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.logoutButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.usernameBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.changeLogin);
+            this.Controls.Add(this.loggedAs);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Voxalia Launcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label loggedAs;
+        private System.Windows.Forms.Button changeLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox usernameBox;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Voxalia/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Net;
+using System.Web;
+using System.Threading;
+
+namespace VoxaliaLauncher
+{
+    public partial class Form1 : Form
+    {
+        public static Encoding encoding = new UTF8Encoding(false);
+
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateLoginDataFromFile();
+        }
+
+        public string UserName = null;
+
+        public void UpdateLoginDataFromFile()
+        {
+            UserName = null;
+            if (File.Exists("logindata.dat"))
+            {
+                try
+                {
+                    UserName = File.ReadAllText("logindata.dat").Split('=')[0].Replace('\n', ' ').Replace('\r', ' ');
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Internal exception reading logindata!" + Environment.NewLine + ex.ToString(), "Error");
+                }
+            }
+            FixButtons();
+        }
+
+        public void FixButtons()
+        {
+            if (UserName == null)
+            {
+                loggedAs.Text = "Logged out";
+                playButton.Enabled = false;
+            }
+            else
+            {
+                loggedAs.Text = "Logged in as: " + UserName;
+                playButton.Enabled = true;
+            }
+        }
+
+        private void changeLogin_Click(object sender, EventArgs e)
+        {
+            GlobalLoginAttempt(usernameBox.Text, passwordBox.Text);
+        }
+
+        public bool Trying = false;
+
+        public const string GlobalServerAddress = "https://frenetic.xyz/";
+
+        public void GlobalLoginAttempt(string user, string pass)
+        {
+            if (Trying)
+            {
+                MessageBox.Show("Already attempting a login...", "Error");
+                return;
+            }
+            Trying = true;
+            changeLogin.Enabled = false;
+            progressBar1.Enabled = true;
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            Task.Factory.StartNew(() =>
+            {
+                using (ShortWebClient wb = new ShortWebClient())
+                {
+                    try
+                    {
+                        NameValueCollection data = new NameValueCollection();
+                        data["formtype"] = "login";
+                        data["username"] = user;
+                        data["password"] = pass;
+                        data["session_id"] = "0";
+                        byte[] response = wb.UploadValues(GlobalServerAddress + "account/micrologin", "POST", data);
+                        string resp = encoding.GetString(response).Trim(' ', '\n', '\r', '\t');
+                        if (resp.StartsWith("ACCEPT=") && resp.EndsWith(";"))
+                        {
+                            string key = resp.Substring("ACCEPT=".Length, resp.Length - 1 - "ACCEPT=".Length);
+                            Invoke(new Action(() =>
+                            {
+                                changeLogin.Enabled = true;
+                                progressBar1.Enabled = false;
+                                progressBar1.Style = ProgressBarStyle.Blocks;
+                                Trying = false;
+                                File.WriteAllText("logindata.dat", user + "=" + key);
+                                UserName = user;
+                                FixButtons();
+                            }));
+                        }
+                        else
+                        {
+                            Invoke(new Action(() =>
+                            {
+                                changeLogin.Enabled = true;
+                                progressBar1.Enabled = false;
+                                progressBar1.Style = ProgressBarStyle.Blocks;
+                                Trying = false;
+                                MessageBox.Show("Login refused: " + resp);
+                                Logout();
+                            }));
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Invoke(new Action(() =>
+                        {
+                            changeLogin.Enabled = true;
+                            progressBar1.Enabled = false;
+                            progressBar1.Style = ProgressBarStyle.Blocks;
+                            Trying = false;
+                            MessageBox.Show("Login failed: " + ex.ToString());
+                            Logout();
+                        }));
+                    }
+                }
+            });
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            Process p = Process.Start("Voxalia.exe");
+            Thread.Sleep(1000);
+            Close();
+        }
+
+        private void logoutButton_Click(object sender, EventArgs e)
+        {
+            Logout();
+        }
+
+        public void Logout()
+        {
+            if (File.Exists("logindata.dat"))
+            {
+                File.Delete("logindata.dat");
+            }
+            UserName = null;
+            FixButtons();
+        }
+    }
+
+    class ShortWebClient : WebClient
+    {
+        protected override WebRequest GetWebRequest(Uri uri)
+        {
+            WebRequest w = base.GetWebRequest(uri);
+            w.Timeout = 30 * 1000;
+            return w;
+        }
+    }
+}
