da99fe76ba0d45f6392e6aeeb6ee058df7cba6a0
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2016-10-19 01:14:39 -0700

    fix simplex noise comments

--- Voxalia/Shared/SimplexNoise.cs
+++ Voxalia/Shared/SimplexNoise.cs
@@ -5,8 +5,42 @@
 // If these are not available, see https://opensource.org/licenses/MIT
 //
 
-namespace Voxalia.Shared
+namespace Voxalia.Shared // mcmonkey - namespace change
 {
+    // mcmonkey - NOTE TO READERS: This class was found elsewhere. Can't locate a link to it.
+    // mcmonkey - Original header is below, though I removed some unicode from the name to avoid file errors.
+    // mcmonkey - ORIGINAL HEADER BEGIN
+
+    // SimplexNoise for C#
+    // Author: Heikki Tormala
+
+    //This is free and unencumbered software released into the public domain.
+
+    //Anyone is free to copy, modify, publish, use, compile, sell, or
+    //distribute this software, either in source code form or as a compiled
+    //binary, for any purpose, commercial or non-commercial, and by any
+    //means.
+
+    //In jurisdictions that recognize copyright laws, the author or authors
+    //of this software dedicate any and all copyright interest in the
+    //software to the public domain. We make this dedication for the benefit
+    //of the public at large and to the detriment of our heirs and
+    //successors. We intend this dedication to be an overt act of
+    //relinquishment in perpetuity of all present and future rights to this
+    //software under copyright law.
+
+    //THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    //EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    //MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    //IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+    //OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+    //ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    //OTHER DEALINGS IN THE SOFTWARE.
+
+    //For more information, please refer to http://unlicense.org/
+
+    // mcmonkey - ORIGINAL HEADER END
+
     // This class all mcmonkey
     public class SimplexNoise
     {
@@ -26,13 +60,13 @@ namespace Voxalia.Shared
         }
     }
 
-    // Below code changed from float to double.
+    // mcmonkey - Below code changed from float to double.
 
     /// <summary>
     /// Implementation of the Perlin simplex noise, an improved Perlin noise algorithm.
     /// Based loosely on SimplexNoise1234 by Stefan Gustavson http://staffwww.itn.liu.se/~stegu/aqsis/aqsis-newnoise/
     /// </summary>
-    public class SimplexNoiseInternal
+    public class SimplexNoiseInternal // mcmonkey - class rename
     {
         /// <summary>
         /// 1D simplex noise
