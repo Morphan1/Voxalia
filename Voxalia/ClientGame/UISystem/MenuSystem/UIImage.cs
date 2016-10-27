c3266b5cd6766e1f91d76220a11f6cf8fb5512fd
Morphan1 <morphanone@gmail.com>
2016-10-26 23:53:43 -0400

    Add lienses
    Gotta have dat liense

--- Voxalia/ClientGame/UISystem/MenuSystem/UIImage.cs
+++ Voxalia/ClientGame/UISystem/MenuSystem/UIImage.cs
@@ -1,4 +1,11 @@
-﻿using OpenTK.Graphics.OpenGL4;
+﻿//
+// This file is part of the game Voxalia, created by FreneticXYZ.
+// This code is Copyright (C) 2016 FreneticXYZ under the terms of the MIT license.
+// See README.md or LICENSE.txt for contents of the MIT license.
+// If these are not available, see https://opensource.org/licenses/MIT
+//
+
+using OpenTK.Graphics.OpenGL4;
 using System;
 using System.Collections.Generic;
 using System.Linq;
